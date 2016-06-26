4be147c5a6694e1134bcb0ddae89009b5101284c
Robert Konrad <git2016@robdangero.us>
2016-06-27 00:18:34 +0200

    Updated std and win32 binary to ed8d92d9e7812111d2945ff0f86290ad9c26ed71.

--- std/hl/_std/String.hx
+++ std/hl/_std/String.hx
@@ -31,11 +31,11 @@ class String {
 	}
 
 	public function toUpperCase() : String {
-		return __alloc__(@:privateAccess bytes.ucs2Upper(0,length<<1), length);
+		return __alloc__(@:privateAccess bytes.ucs2Upper(0,length), length);
 	}
 
 	public function toLowerCase() : String {
-		return __alloc__(@:privateAccess bytes.ucs2Lower(0,length<<1), length);
+		return __alloc__(@:privateAccess bytes.ucs2Lower(0,length), length);
 	}
 
 	public function charAt(index : Int) : String {
@@ -170,6 +170,11 @@ class String {
 			return __alloc__(b, 2); // UTF16 encoding but UCS2 API (same as JS)
 		} else
 			throw "Invalid unicode char " + code;
+	}
+
+	function toUtf8() : hl.types.Bytes {
+		var size = 0;
+		return bytes.utf16ToUtf8(0, size);
 	}
 
 	@:keep function __string() : hl.types.Bytes {
