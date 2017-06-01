c3db4df6a43298e187f72fe3c9c5b99a8dbaf42c
Robert Konrad <git2016@robdangero.us>
2017-06-01 14:18:25 +0200

    Use kha.Scheduler in haxe.Timer

--- std/haxe/Timer.hx
+++ std/haxe/Timer.hx
@@ -37,14 +37,7 @@ package haxe;
 **/
 class Timer {
 
-	#if (flash || js)
-		private var id : Null<Int>;
-	#elseif java
-		private var timer : java.util.Timer;
-		private var task : java.util.TimerTask;
-	#else
-		private var event : MainLoop.MainEvent;
-	#end
+	private var id : Null<Int>;
 
 	/**
 		Creates a new timer that will run every `time_ms` milliseconds.
@@ -58,23 +51,8 @@ class Timer {
 		The accuracy of this may be platform-dependent.
 	**/
 	public function new( time_ms : Int ){
-		#if flash
-			var me = this;
-			id = untyped __global__["flash.utils.setInterval"](function() { me.run(); },time_ms);
-		#elseif js
-			var me = this;
-			id = untyped setInterval(function() me.run(),time_ms);
-		#elseif java
-			timer = new java.util.Timer();
-			timer.scheduleAtFixedRate(task = new TimerTask(this), haxe.Int64.ofInt(time_ms), haxe.Int64.ofInt(time_ms));
-		#else
-			var dt = time_ms / 1000;
-			event = MainLoop.add(function() {
-				@:privateAccess event.nextRun += dt;
-				run();
-			});
-			event.delay(dt);
-		#end
+		var me = this;
+		id = kha.Scheduler.addTimeTask(function() me.run(), time_ms / 1000, time_ms / 1000);
 	}
 
 	/**
@@ -86,27 +64,10 @@ class Timer {
 		It is not possible to restart `this` Timer once stopped.
 	**/
 	public function stop() {
-		#if (flash || js)
-			if( id == null )
-				return;
-			#if flash
-				untyped __global__["flash.utils.clearInterval"](id);
-			#elseif js
-				untyped clearInterval(id);
-			#end
-			id = null;
-		#elseif java
-			if(timer != null) {
-				timer.cancel();
-				timer = null;
-			}
-			task = null;
-		#else
-			if( event != null ) {
-				event.stop();
-				event = null;
-			}
-		#end
+		if( id == null )
+			return;
+		kha.Scheduler.removeTimeTask(id);
+		id = null;
 	}
 
 	/**
@@ -167,37 +128,7 @@ class Timer {
 		between two values make sense.
 	**/
 	public static inline function stamp() : Float {
-		#if flash
-			return flash.Lib.getTimer() / 1000;
-		#elseif (neko || php)
-			return Sys.time();
-		#elseif js
-			return Date.now().getTime() / 1000;
-		#elseif cpp
-			return untyped __global__.__time_stamp();
-		#elseif python
-			return Sys.cpuTime();
-		#elseif sys
-			return Sys.time();
-
-		#else
-			return 0;
-		#end
+		return kha.Scheduler.realTime();
 	}
 
 }
-
-#if java
-@:nativeGen
-private class TimerTask extends java.util.TimerTask {
-	var timer:Timer;
-	public function new(timer:Timer):Void {
-		super();
-		this.timer = timer;
-	}
-
-	@:overload override public function run():Void {
-		timer.run();
-	}
-}
-#end
